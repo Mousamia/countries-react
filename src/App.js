f68d368258f62694563345747a1bf163f7687175
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-03-30 17:17:48 +0600

    file setup for spa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Cosmetics from './components/Cosmetics/Cosmetics';
 
 
 function App() {
   return (
     <div className="App">
      <h1>Welocme to my Product</h1>
-     <Cosmetics></Cosmetics>
     </div>
   );
 }
